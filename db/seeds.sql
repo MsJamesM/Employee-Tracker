e5aafad0a62cd03de14e30bc067d6bb8fc5fd1f7
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-06-26 17:27:22 -0600

    creating access to SQL data via terminal prompts

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,33 @@
 INSERT INTO department (name)
 VALUES ("Management"),
+        ("Customer Service and Relations"),
         ("Sales"),
-        ("Accounting"),
-        ("Customer Service and Relations");
+        ("Accounting");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Regional Manager", 100000, 1),
+VALUES ("Accountant", 77000, 6),
     ("Assistant to the Regional Manager", 75000, 2),
+    ("Customer Service Specialist", 69000, 9),
+    ("Head of Accounting", 78000, 5),
+    ("Quality Assurance", 70000, 7),
+    ("Regional Manager", 100000, 1),
     ("Receptionist", 55000, 3),
     ("Salesperson", 75000, 4),
-    ("Head of Accounting", 78000, 5),
-    ("Accountant", 77000, 6),
-    ("Quality Assurance", 70000, 7),
     ("Supplier Relations", 69000, 8),
-    ("Customer Service Specialist", 69000, 9),
     ("Warehouse Foreman", 69000, 10);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Michael", "Scott", 1, NULL),
-    ("Dwight", "Schrute", 2, 1),
-    ("Pamela", "Beesly", 3, 1),
-    ("Jim", "Halpert", 4, 1),
-    ("Andy", "Bernard", 4, 1),
-    ("Stanley", "Hudson", 4, 1),
-    ("Phyllis", "Vance", 4, 1),
+VALUES ("Andy", "Bernard", 4, 1),
     ("Angela", "Martin", 5, 1),
-    ("Oscar", "Martinez", 6, 1),
-    ("Kevin", "Malone", 6, 1),
     ("Creed", "Bratton", 7, 1),
-    ("Meredith", "Palmer", 8, 1),
+    ("Daryll", "Philbin", 10, 1),
+    ("Dwight", "Schrute", 2, 1),
+    ("Jim", "Halpert", 4, 1),
+    ("Kevin", "Malone", 6, 1),
     ("Kelly", "Kapoor", 9, 1),
-    ("Daryll", "Philbin", 10, 1);
+    ("Meredith", "Palmer", 8, 1),
+    ("Michael", "Scott", 1, NULL),
+    ("Oscar", "Martinez", 6, 1),
+    ("Pamela", "Beesly", 3, 1),
+    ("Phyllis", "Vance", 4, 1),
+    ("Stanley", "Hudson", 4, 1);
--- server.js
+++ server.js
@@ -1,34 +1,50 @@
-require("dotenv").config();
-const mysql = require("mysql2");
+const mysql = require("mysql");
 const inquirer = require("inquirer");
+require("dotenv").config();
 
-const db = mysql.createConnection(
-  {
-    host: process.env.HOST,
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-  },
-  console.log(`Connected to the employee database.`)
-);
+// connecting
+const db = mysql.createConnection({
+  host: "localhost",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "employees_db",
+});
 
-// Connect to the database
-connection.connect((err) => {
+db.connect((err) => {
   if (err) {
-    console.error("Error connecting to the database:", err);
+    console.error("There was an error connecting to the database");
     return;
   }
-  console.log("Connected to the database");
-
-  // Read the seeds.sql file and execute the queries
-  const fs = require("fs");
-  const seedsSQL = fs.readFileSync("seeds.sql", "utf8");
-
-  connection.query(seedsSQL, (err, results) => {
-    if (err) {
-      console.error("Error executing seeds.sql:", err);
-    } else {
-      console.log("Seeds executed successfully");
-    }
-  });
+  console.log("Successfully connected to the database");
+  init();
 });
+
+// prompts
+async function init() {
+  const { choicesMenu } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "choicesMenu",
+      message: "Please choose from the list",
+      choices: [{ name: "View all employees", value: "viewAllEmployees" }],
+    },
+  ]);
+
+  switch (choicesMenu) {
+    case "viewAllEmployees":
+      viewAllEmployees();
+      break;
+  }
+}
+
+// results
+function viewAllEmployees() {
+  db.query("SELECT * FROM employee", (err, results) => {
+    if (err) {
+      console.error("There was an error viewing employees");
+      return;
+    }
+    console.table(results);
+    init();
+  });
+}
